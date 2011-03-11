9e11daf695e4cb50c4a3b6ca1f3853f7552dccb9
Tim Su <tim@todoroo.com>
2011-03-10 18:16:16 -0800

    Hand-converted generator to rails 3

--- generators/apn_migrations_generator.rb
+++ generators/apn_migrations_generator.rb
@@ -1,31 +1,30 @@
-require 'rails_generator'
 # Generates the migrations necessary for APN on Rails.
 # This should be run upon install and upgrade of the 
 # APN on Rails gem.
 # 
 #   $ ruby script/generate apn_migrations
-class ApnMigrationsGenerator < Rails::Generator::Base
+
+require 'fileutils'
+
+class ApnMigrationsGenerator 
   
-  def manifest # :nodoc:
-    record do |m|
-      timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
-      db_migrate_path = File.join('db', 'migrate')
+  def generate
+    timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+    db_migrate_path = File.join('db', 'migrate')
+    mkdir_p(db_migrate_path);
       
-      m.directory(db_migrate_path)
-      
-      Dir.glob(File.join(File.dirname(__FILE__), 'templates', 'apn_migrations', '*.rb')).sort.each_with_index do |f, i|
-        f = File.basename(f)
-        f.match(/\d+\_(.+)/)
-        timestamp = timestamp.succ
-        if Dir.glob(File.join(db_migrate_path, "*_#{$1}")).empty?
-          m.file(File.join('apn_migrations', f), 
-                 File.join(db_migrate_path, "#{timestamp}_#{$1}"), 
-                 {:collision => :skip})
-        end
+    Dir.glob(File.join(File.dirname(__FILE__), 'templates', 'apn_migrations', '*.rb')).sort.each_with_index do |f, i|
+      f = File.basename(f)
+      f.match(/\d+\_(.+)/)
+      timestamp = timestamp.succ
+      if Dir.glob(File.join(db_migrate_path, "*_#{$1}")).empty?
+        FileUtils.copy(File.join('templates/apn_migrations', f), 
+                     File.join(db_migrate_path, "#{timestamp}_#{$1}"))
       end
+    end
+
+    print "Dumped stuff to #{db_migrate_path}\n"
       
-    end # record
-    
   end # manifest
   
 end # ApnMigrationsGenerator

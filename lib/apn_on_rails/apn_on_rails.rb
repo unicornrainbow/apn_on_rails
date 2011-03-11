5c01a53bde3fcd63afa17093ac76ef1ecd7d53a5
Tim Su <tim@todoroo.com>
2011-03-10 19:29:44 -0800

    really get apn on rails working for rails 3

--- lib/apn_on_rails/apn_on_rails.rb
+++ lib/apn_on_rails/apn_on_rails.rb
@@ -1,39 +1,46 @@
 require 'socket'
 require 'openssl'
 require 'configatron'
-
-rails_root = File.join(FileUtils.pwd, 'rails_root')
-if defined?(Rails.root)
-  rails_root = Rails.root.to_s
-end
-
-rails_env = 'development'
-if defined?(Rails.env)
-  rails_env = Rails.env
-end
-
-configatron.apn.set_default(:passphrase, '')
-configatron.apn.set_default(:port, 2195)
-
-configatron.apn.feedback.set_default(:passphrase, configatron.apn.passphrase)
-configatron.apn.feedback.set_default(:port, 2196)
-
-if rails_env == 'production'
-  configatron.apn.set_default(:host, 'gateway.push.apple.com')
-  configatron.apn.set_default(:cert, File.join(rails_root, 'config', 'apple_push_notification_production.pem'))
-
-  configatron.apn.feedback.set_default(:host, 'feedback.push.apple.com')
-  configatron.apn.feedback.set_default(:cert, configatron.apn.cert)
-else
-  configatron.apn.set_default(:host, 'gateway.sandbox.push.apple.com')
-  configatron.apn.set_default(:cert, File.join(rails_root, 'config', 'apple_push_notification_development.pem'))
-
-  configatron.apn.feedback.set_default(:host, 'feedback.sandbox.push.apple.com')
-  configatron.apn.feedback.set_default(:cert, configatron.apn.cert)
-end
+require 'rails'
 
 module APN # :nodoc:
 
+  class Railtie < Rails::Railtie
+
+    initializer 'apn.configatron' do
+      rails_root = File.join(FileUtils.pwd, 'rails_root')
+      if defined?(::Rails.root)
+        rails_root = ::Rails.root.to_s
+      end
+
+      rails_env = 'development'
+      if defined?(::Rails.env)
+        rails_env = ::Rails.env
+      end
+
+      configatron.apn.set_default(:passphrase, '')
+      configatron.apn.set_default(:port, 2195)
+
+      configatron.apn.feedback.set_default(:passphrase, configatron.apn.passphrase)
+      configatron.apn.feedback.set_default(:port, 2196)
+
+      if rails_env == 'production'
+        configatron.apn.set_default(:host, 'gateway.push.apple.com')
+        configatron.apn.set_default(:cert, File.join(rails_root, 'config', 'apple_push_notification_production.pem'))
+
+        configatron.apn.feedback.set_default(:host, 'feedback.push.apple.com')
+        configatron.apn.feedback.set_default(:cert, configatron.apn.cert)
+      else
+        configatron.apn.set_default(:host, 'gateway.sandbox.push.apple.com')
+        configatron.apn.set_default(:cert, File.join(rails_root, 'config', 'apple_push_notification_development.pem'))
+
+        configatron.apn.feedback.set_default(:host, 'feedback.sandbox.push.apple.com')
+        configatron.apn.feedback.set_default(:cert, configatron.apn.cert)
+      end
+    end
+
+  end
+  
   module Errors # :nodoc:
 
     # Raised when a notification message to Apple is longer than 256 bytes.
